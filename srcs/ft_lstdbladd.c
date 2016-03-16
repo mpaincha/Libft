d2f2a1717326e26aec096f9f74a84f8c4dc83281
Maelle PAINCHAULT <mpaincha@e2r4p8.42.fr>
2016-03-16 14:04:00 +0100

    modification fonctin lst

--- srcs/ft_lstdbladd.c
+++ srcs/ft_lstdbladd.c
@@ -12,6 +12,34 @@
 
 #include "../includes/libft.h"
 
+void	ft_lstdbladd(t_dbllist *list, void *content, size_t cont_size)
+{
+	t_elem		*new_elem;
+
+	new_elem = (t_elem *)malloc(sizeof(t_elem));
+	if (new_elem == NULL)
+		return ;
+	ft_bzero(new_elem, sizeof(t_elem));
+	new_elem->content = (void *)malloc(cont_size);
+	if (new_elem->content == NULL)
+	{
+		free(new_elem);
+		return ;
+	}
+	ft_memcpy(new_elem->content, content, cont_size);
+	new_elem->next = NULL;
+	new_elem->prev = NULL;
+	if (list->head == NULL)
+		list->head = new_elem;
+	else
+	{
+		list->tail->next = new_elem;
+		new_elem->prev = list->tail;
+	}
+	list->tail = new_elem;
+	list->length++;
+}
+
 void	ft_lstdbladd_head(t_dbllist *list, void *content, size_t cont_size)
 {
 	t_elem		*new_elem;
